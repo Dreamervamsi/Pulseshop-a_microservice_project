af9ce2e8aa84be9e8d50b5d04969f6ecba9d69f0
vamsi krishna <kvamsi.nellore@gmail.com>
2026-06-06 11:21:07 +0530

    Update redis logic for otp veerfication

--- services/auth/src/utils/auth.helper.ts
+++ services/auth/src/utils/auth.helper.ts
@@ -4,6 +4,7 @@ import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 import redis from "@pulseshop/shared/index";
 import { sendEmail } from "./sendEmail";
+import {NextFunction} from 'express';
 
 export async function checkUserExists(email:string){
     const isUserExists = await prisma.user.findUnique({
@@ -28,9 +29,59 @@ export const createUser = async(name:string,email:string,hashedPassword:string)=
     return {user,token};
 }
 
+export const trackOtpRequest = async(email:string,next:NextFunction)=>{
+    const otpReqCount = parseInt(await redis.get(`otp-req-count:${email}`) || '0');
+    if(otpReqCount>=2)
+    {
+        await redis.set(`otp-spam-lock:${email}`,"locked","EX",3600);
+        return next(new BadRequestError("Too many requests! Please try again after 1hour"));
+    }
+    await redis.set(`otp-req-count:${email}`, otpReqCount + 1, "EX", 3600);
+    return next();
+}
+
+export const trackOtpAttempts = async(email:string,otp:Number,next:NextFunction)=>{
+    const isLocked = await redis.get(`otp-lock:${email}`);
+    if(isLocked == 'locked')
+    {
+        return next(new BadRequestError("Multiple failed attempts! Please wait for 30 minutes."));
+    }
+
+    const otpAttemptCount = parseInt(await redis.get(`otp-attempts:${email}`) || '0');
+    const storedOtp = parseInt(await redis.get(`otp:${email}`) || '0');
+    if(storedOtp != otp){
+        if(otpAttemptCount >= 3){
+            await redis.set(`otp-lock:${email}`,"locked","EX",1800);
+            return next(new BadRequestError("Multiple failed attempts! please try again after 30minutes."));
+        }
+        await redis.set(`otp-attempts:${email}`,otpAttemptCount+1,"EX",1800);
+        return next(new BadRequestError(`Incorrect OTP! You have ${3-otpAttemptCount} attempts left`));
+    }
+    await redis.del(`otp:${email}`);
+    await redis.del(`otp-attempts:${email}`);
+    await redis.del(`otp-cooldown:${email}`);
+    return next();
+    
+}
+
+export const validateOtp = async(email:string,next:NextFunction)=>{
+    if(await redis.get(`otp-spam-lock:${email}`))
+    {
+        return next(new BadRequestError("Too many otp requeests! Please wait for 1hour to generate new OTP"));
+    }
+    if(await redis.get(`otp-lock:${email}`))
+    {
+        return next(new BadRequestError("Multiple failed attempts! Please wait for 30 minutes for requesting new OTP"));
+    }
+    if(await redis.get(`otp-cooldown:${email}`)){
+        return next(new BadRequestError("Please wait for 1min to request for new Otp"));
+    }
+    return next();
+}
+
 export const sendOtp = async(to:string,message:string,email:string)=>{
     const otp = crypto.randomInt(1000,9999).toString();
     sendEmail(to,message);
     await redis.set(`otp:${email}`,otp,'EX',300);
-    await redis.set(`otp_cooldown:${email}`,'true','EX',60);
+    await redis.set(`otp-cooldown:${email}`,'true','EX',60);
 }
